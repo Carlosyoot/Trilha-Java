16ff34548246f6ff9fed6304662a410b87974e2b
Carlosyoot <carlosdanielyoot@gmail.com>
2024-06-20 18:14:39 -0300

    Commit Tipos variaveis

--- tipos-variaveis/src/Tipos_Variaveis.java
+++ tipos-variaveis/src/Tipos_Variaveis.java
@@ -1,5 +1,26 @@
+
+/* Tipos primitivos de variáveis
+ * 
+ * Int;Byte;Short;Long;Boolean;Float;Double;Char
+ * 
+ * Byte = 4 bits
+ * Short = 8 bits
+ * Int = 16 bits
+ * long = 32 bits
+ * float = 16 bits
+ * double = 32 bits
+ * 
+ * Java é fortemente tipado, respeitando o limites e normas da variavel
+ * 
+ * 
+ * short numeroCurto = 1;
+ * int numeroNormal = numeroCurto; 
+ * short numeroCurto2 = numeroNormal;
+ * 
+ * resolução:
+   short numeroCurto2 = (short)numeroNormal;
+ */
+
 public class Tipos_Variaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    
 }
